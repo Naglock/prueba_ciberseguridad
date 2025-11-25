59b5536654c3903ccfecf02d6cef824c151aaa3b
Naglock <iv.huentemilla@duocuc.cl>
2025-11-25 09:33:52 -0300

    cambio para la base de datos

--- vulnerable_app.py
+++ vulnerable_app.py
@@ -10,17 +10,17 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
 
-def get_db_connection():
+###def get_db_connection():
 
-    db_config = {
-        'host': 'localhost',  # El servidor local de XAMPP
-        'user': 'root',       # El usuario de MySQL
-        'password': '',       # Tu contraseña (si no tienes una, déjala vacía)
-        'database': 'prueba' # El nombre de la base de datos
-    }
-    conn = mysql.connector.connect(**db_config)
-
-    return conn
+#    db_config = {
+#        'host': 'localhost',  # El servidor local de XAMPP
+ #       'user': 'root',       # El usuario de MySQL
+#        'password': '',       # Tu contraseña (si no tienes una, déjala vacía)
+#        'database': 'prueba' # El nombre de la base de datos
+#    }
+#    conn = mysql.connector.connect(**db_config)
+#
+ #   return conn
 
 
 def hash_password(password):
